a9bbb5c69e53685d52c655bddb3cbfd2cb6e1539
Ben Grisdale <bengris32@protonmail.ch>
2026-08-02 23:09:15 +0100

    Update dist script

--- makedist.sh
+++ makedist.sh
@@ -2,35 +2,14 @@
 
 rm -rf dist
 
-mkdir -p dist/unlock/kamakiri/bin
-cp bin/{preloader.img,lk.bin,tz.img,twrp.img,microloader.bin} dist/unlock/kamakiri/bin/
-
-mkdir -p dist/unlock/kamakiri/lk-payload/build
-cp lk-payload/build/payload.bin dist/unlock/kamakiri/lk-payload/build/
-
-mkdir -p dist/unlock/kamakiri/modules
-cp modules/{common.py,load_payload.py,logger.py,main.py,functions.py} dist/unlock/kamakiri/modules/
-
-mkdir -p dist/unlock/kamakiri/brom-payload/stage1
-cp brom-payload/stage1/stage1.bin dist/unlock/kamakiri/brom-payload/stage1/
-
-mkdir -p dist/unlock/kamakiri/brom-payload/stage2
-cp brom-payload/stage2/stage2.bin dist/unlock/kamakiri/brom-payload/stage2/
-
-cp {bootrom-step.sh,fastboot-step.sh} dist/unlock/kamakiri/
-
-mkdir -p dist/unlock/META-INF/com/google/android
-cp META-INF/com/google/android/{update-binary,updater-script} dist/unlock/META-INF/com/google/android/
-
-#mkdir -p dist/stock/kamakiri/bin
-#cp bin/{boot.img,recovery.img} dist/stock/kamakiri/bin/
-#cp return-to-stock.sh dist/stock/kamakiri/
-
-mkdir -p dist/gptfix/kamakiri/bin
-cp gpt-fix.sh dist/gptfix/kamakiri/
-cp bin/gpt-mantis.bin dist/gptfix/kamakiri/bin/
-
-mkdir -p dist/full
-cp -r dist/unlock/* dist/full/
-#cp -r dist/stock/* dist/full/
-cp -r dist/gptfix/* dist/full/
+mkdir -p dist/kamakiri/brom-payload/stage1 dist/kamakiri/brom-payload/stage2 dist/kamakiri/modules
+cp bootrom-step.sh fastboot-step.sh gpt-fix.sh dist/kamakiri/
+cp -r bin dist/kamakiri
+cp -r brom-payload/stage1/stage1.bin dist/kamakiri/brom-payload/stage1
+cp -r brom-payload/stage2/stage2.bin dist/kamakiri/brom-payload/stage2
+cp -r modules/*.py dist/kamakiri/modules
+cp requirements.txt dist/kamakiri
+cp -r META-INF dist/
+cd dist/
+zip -r kamakiri-cupcake.zip *
+cd ..
